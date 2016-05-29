51c0564d6bbc4d4c2478606edcb36e48e92e616f
Noam Postavsky <npostavs@users.sourceforge.net>
2016-05-29 00:37:49 -0400

    Add new submodule section type
    Define visiting and deletion actions for it.

--- lisp/magit-submodule.el
+++ lisp/magit-submodule.el
@@ -174,7 +174,7 @@ For each section insert the path and the output of `git describe --tags'."
           (dolist (module modules)
             (let ((default-directory
                     (expand-file-name (file-name-as-directory module))))
-              (magit-insert-section (file module t)
+              (magit-insert-section (submodule module t)
                 (insert (propertize (format col-format module)
                                     'face 'magit-diff-file-heading))
                 (if (not (file-exists-p ".git"))
@@ -231,6 +231,38 @@ These sections can be expanded to show the respective commits."
                               'modules-unpushed-to-pushremote
                               'magit-get-push-branch
                               "%s..HEAD"))
+
+(defun magit-submodule-visit (module &optional other-window)
+  "Visit MODULE by calling `magit-status' on it.
+Offer to initialize MODULE if it's not checked out yet."
+  (interactive (list (or (magit-section-when module)
+                         (user-error "No submodule at point"))
+                     current-prefix-arg))
+  (let ((path (expand-file-name module)))
+    (if (or (file-exists-p (expand-file-name ".git" module))
+            (not (y-or-n-p (format "Setup submodule '%s' first?"
+                                   module))))
+        (magit-diff-visit-directory path other-window)
+      (magit-submodule-setup module)
+      (set-process-sentinel
+       (lambda (process event)
+         (when (memq (process-status process) '(exit signal))
+           (let ((magit-process-raise-error t))
+             (magit-process-sentinel process event)))
+         (when (and (eq (process-status process) 'exit)
+                    (= (process-exit-status process) 0))
+           (magit-diff-visit-directory path other-window)))))))
+
+(defvar magit-submodule-section-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [C-return] 'magit-submodule-visit)
+    (define-key map "\C-j"     'magit-submodule-visit)
+    (define-key map [remap magit-visit-thing]  'magit-submodule-visit)
+    (define-key map [remap magit-delete-thing] 'magit-submodule-deinit)
+    (define-key map "K" 'magit-file-untrack)
+    (define-key map "R" 'magit-file-rename)
+    map)
+  "Keymap for `submodule' sections.")
 
 (defun magit--insert-modules-logs (heading type fn format)
   "For internal use, don't add to a hook."
